ac621926549a935e3a1a50e6f00f7b94e2ae24d3
jason <j@meom.moe>
2018-03-14 21:54:53 +0800

    Add spelling category

--- papersmith/editor/issue.py
+++ papersmith/editor/issue.py
@@ -5,7 +5,7 @@ class Issue:
     
     def __init__(self, category, itype, start, end, replacement, exp_id):
         '''
-        category:       1语法/2语义/3句式
+        category:       1拼写/2语法/3语义/4句式
         itype:          issue 类型：1错误/2建议/3普通；
         start:          起始下标，列表
         end:            终止下标，列表
@@ -21,9 +21,10 @@ class Issue:
 
     def print(self):
         '用于调试打印'
-        if self.category == 1:      print('grammar', end = ', ')
-        elif self.category == 2:    print('semantic', end = ', ')
-        elif self.category == 3:    print('sentence', end = ', ')
+        if self.category == 1:      print('spelling', end = ', ')
+        elif self.category == 2:    print('grammar', end = ', ')
+        elif self.category == 3:    print('semantic', end = ', ')
+        elif self.category == 4:    print('sentence', end = ', ')
         else:                       print('unknown category', end = ', ')
 
         if self.itype == 1:      print('error', end = ', ')
