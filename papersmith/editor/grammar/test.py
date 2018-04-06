452a09c5215d85d76039f98dc588a8d02d7df886
JerryXu0129 <1071260758@qq.com>
2018-04-06 13:07:13 +0800

    sadfdsa

--- papersmith/editor/grammar/test.py
+++ papersmith/editor/grammar/test.py
@@ -6,9 +6,9 @@ def check(content):
 	sentence_tag = nltk.pos_tag(nltk.word_tokenize(content))
 	grammar = r"""
 	NP: {<.*>+}
-		}<VBD|IN>+{
+		}<VBD|IN|,|.>+{
 	"""
 	tree = nltk.RegexpParser(grammar).parse(sentence_tag)
 	print(tree)
 
-check("yesterday I went to the theater.")
+check("I went to the theater.")
