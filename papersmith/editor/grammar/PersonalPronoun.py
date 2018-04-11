86ab5ba6f8beefd399194fa92d71fff8e038acd1
jszh <j@meom.moe>
2018-04-11 22:02:45 +0800

    修改了换行句首大写的bug (#13)
    * 修改了换行句首大写的bug

---
+++ papersmith/editor/grammar/PersonalPronoun.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+from papersmith.editor.issue import Issue
+
+def check(content):
+    '检查内容中的语法错误'
+    PRPlist=[['I','me','my','mine'],['you','you','your','yours'],['he','him','his','his'],['she','her','her','hers'],['we','us','our','ours'],['they','them','their','theirs'],['it','it','its','its']]
+    
+
+    issues = []
+
+    return issues # List of issues
