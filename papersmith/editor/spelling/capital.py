516674e6f117733699e6d4d110bcdefb99a2820c
JerryXu0129 <1071260758@qq.com>
2018-04-11 22:00:06 +0800

    修改了换行句首大写的bug

--- papersmith/editor/spelling/capital.py
+++ papersmith/editor/spelling/capital.py
@@ -21,16 +21,24 @@ def check(content):
             issues.append(Issue(1,1,[j],[j+l+1],string,0))
             break
         break
+    yinhao_num=0
+    if content[0]=="'" or content[0]=='\"':
+        yinhao_num+=1
     for i in range(1,len(content)):
         if content[i] =='!' or content[i] =='?' or (content[i] =='.'and not content[i-1].isupper()):
-            isYinhao=0
-            for j in range(5):
+            huanhang=0
+            yinhao=0
+            for j in range(10):
                 if i+j+1>=len(content):
                     break
-                if content[i+j+1]=='\'' or content[i+j+1]=='\"':
-                    isYinhao=1
-                    continue
                 if content[i+j+1]==' ':
+                    continue
+                if content[i+j+1]=='\n':
+                    huanhang=1
+                    continue
+                if content[i+j+1]=='\'' or content[i+j+1]=='\"':
+                    yinhao=1
+                    yinhao_num+=1
                     continue
                 if ord(content[i+j+1])>96 and ord(content[i+j+1])<123:
                     w=''
@@ -39,12 +47,12 @@ def check(content):
                         w+=content[i+j+k+1]
                         if not((ord(content[i+j+k+2])>96 and ord(content[i+j+k+2])<123) or (ord(content[i+j+k+2])>64 and ord(content[i+j+k+2])<91) or content[i+j+k+2]=="'"):
                             break
-                    for k in range(10):
+                    for k in range(30):
                         if (ord(content[i+j+k+len(w)+2])>96 and ord(content[i+j+k+len(w)+2])<123):
                             ww+=content[i+j+k+len(w)+2]
                         else:
                             break
-                    if isYinhao==0 or not ( w in s or ww in s):
+                    if yinhao==0 or huanhang==1 or yinhao_num%2==1 or not ( w in s or ww in s):
                         string=chr(ord(content[i+j+1])-32)
                         for k in range(len(w)-1):
                             string+=content[i+j+k+2]
