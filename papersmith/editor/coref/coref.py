ca5b5ca0a04b02dc55d6a7067705d05f9b98f1bc
Mrhelium <harryinhurry@qq.com>
2018-04-12 08:55:12 +0800

    loading model

--- papersmith/editor/coref/coref.py
+++ papersmith/editor/coref/coref.py
@@ -4,22 +4,38 @@ from neuralcoref import Coref
 import os
 import json
 import sys, getopt
+import spacy
+
 # from papersmith.editor.issue import Issue
 
-pronoun = ["I", "me", "my", "mine", "myself", "you", "your", "yours", "yourself", "we", "us", "our", "ours", "ourselves", "yourselves",
-			"I", "Me", "My", "Mine", "Myself", "You", "Your", "Yours", "Yourself", "We", "Us", "Our", "Ours", "Ourselves", "Yourselves",
+pronoun = ["I", "me", "my", "mine", "myself", "you", "your", "yours", "yourself", "we", "us", "our", "ours",
+           "ourselves", "yourselves",
+           "I", "Me", "My", "Mine", "Myself", "You", "Your", "Yours", "Yourself", "We", "Us", "Our", "Ours",
+           "Ourselves", "Yourselves",
            ]
 
-notpro=[]
+notpro = []
 
-def coref(_content):
+
+def coref(_content, nlp=None):
 	import re
-	coref = Coref()
-	content=_content.replace("\n",'')
+
+	if nlp is None:
+		print("Loading spacy model")
+		try:
+			spacy.info('en_core_web_sm')
+			model = 'en_core_web_sm'
+		except IOError:
+			print("No spacy 2 model detected, using spacy1 'en' model")
+			model = 'en'
+		nlp = spacy.load(model)
+
+	coref = Coref(nlp)
+	content = _content.replace("\n", '')
 	clusters = coref.continuous_coref(
 		utterances=content)
 	clu = coref.get_clusters(remove_singletons=False)
-	utterances=coref.get_utterances()
+	utterances = coref.get_utterances()
 	print(utterances)
 	result = []
 	mentions = coref.get_mentions()
@@ -73,7 +89,7 @@ def coref(_content):
 				else:
 					notpro.append(id)
 
-	print(notpro,result)
+	print(notpro, result)
 	for id in score['pair_scores']:
 		first = (0, 0)
 		second = (0, 0)
@@ -84,21 +100,22 @@ def coref(_content):
 			elif pairvalue > second[1]:
 				second = (pair, pairvalue)
 		if first[1] > 0 and second[1] > 0 and first[1] - second[1] < 2.8:
-			print(id, first[1],second[1])
-			print(first[0],second[0])
-			firstid=first[0]
-			secondid=second[0]
-			if firstid<secondid:
-				tempid=firstid
-				firstid=secondid
-				secondid=tempid
-			if score['pair_scores'][firstid][secondid]>2 or firstid-secondid>15 \
-					or indexs[firstid+1][1]==indexs[secondid+1][1] or indexs[firstid+1][0]==indexs[secondid+1][0] \
+			print(id, first[1], second[1])
+			print(first[0], second[0])
+			firstid = first[0]
+			secondid = second[0]
+			if firstid < secondid:
+				tempid = firstid
+				firstid = secondid
+				secondid = tempid
+			if score['pair_scores'][firstid][secondid] > 2 or firstid - secondid > 15 \
+					or indexs[firstid + 1][1] == indexs[secondid + 1][1] or indexs[firstid + 1][0] == \
+					indexs[secondid + 1][0] \
 					or firstid in notpro or secondid in notpro:
 				print('reject')
 			else:
 				result.append([indexs[id + 1], indexs[first[0] + 1], indexs[second[0] + 1]])
-		if mentions[id].mention_type==0 and first[1] < 2:
+		if mentions[id].mention_type == 0 and first[1] < 2:
 			if str(mentions[id]) != 'It' and str(mentions[id]) != 'it' and str(mentions[id]) not in pronoun:
 				result.append([indexs[id + 1]])
 		print(id, score['pair_scores'][id])
@@ -111,23 +128,23 @@ def coref(_content):
 	print(results)
 
 
-
-	# startlist = []
-	# endlist = []
-	# issues = []
-	# for lists in results:
-	# 	for cor in lists:
-	# 		startlist.append(cor[0])
-	# 		endlist.append(cor[1])
-	# 	issue = Issue(1, 1, startlist, endlist, 'wrong', 0)
-	# 	issues.append(issue)
-	# 	startlist = []
-	# 	endlist = []
-	# return issues
+# startlist = []
+# endlist = []
+# issues = []
+# for lists in results:
+# 	for cor in lists:
+# 		startlist.append(cor[0])
+# 		endlist.append(cor[1])
+# 	issue = Issue(1, 1, startlist, endlist, 'wrong', 0)
+# 	issues.append(issue)
+# 	startlist = []
+# 	endlist = []
+# return issues
 
 
 if __name__ == "__main__":
-	coref("Various implementations have been available, currently S-PLUS, a commercial system from the Insightful Corporation1 in Seattle, and R,2 an Open Source system written by a team of volunteers. Both can be run on Windows and a range of UNIX / Linux operating systems: R also runs on Macintoshes. It became the statistician's calculator for the 1990s, allowing easy access to the computing power and graphical capabilities of modern workstations and personal computers. This is the fourth edition of a book which first appeared in 1994, and the S environment has grown rapidly since. This book concentrates on using the current systems to do statistics; there is a companion volume  which discusses programming in the S language in much greater depth. Some of the more specialized functionality of the S environment is covered in on-line complements, additional sections and chapters which are available on the World Wide Web. The datasets and S functions that we use are supplied with most S environments and are also available on-line. This is not a text in statistical theory, but does cover modern statistical methodology.")
+	coref(
+		"Various implementations have been available, currently S-PLUS, a commercial system from the Insightful Corporation1 in Seattle, and R,2 an Open Source system written by a team of volunteers. Both can be run on Windows and a range of UNIX / Linux operating systems: R also runs on Macintoshes. It became the statistician's calculator for the 1990s, allowing easy access to the computing power and graphical capabilities of modern workstations and personal computers. This is the fourth edition of a book which first appeared in 1994, and the S environment has grown rapidly since. This book concentrates on using the current systems to do statistics; there is a companion volume  which discusses programming in the S language in much greater depth. Some of the more specialized functionality of the S environment is covered in on-line complements, additional sections and chapters which are available on the World Wide Web. The datasets and S functions that we use are supplied with most S environments and are also available on-line. This is not a text in statistical theory, but does cover modern statistical methodology.")
 
 # They like Bill and Jack. He is very tall. He is handsome. He saw Jack, the younger child, who is a singer. What is that? It is her friend who loves a dog. Its name is Mike.
 # It became the statistician's calculator for the 1990s, allowing easy access to the computing power and graphical capabilities of modern workstations and personal computers. Various implementations have been available, currently S-PLUS, a commercial system from the Insightful Corporation1 in Seattle, and R,2 an Open Source system written by a team of volunteers. Both can be run on Windows and a range of UNIX / Linux operating systems: R also runs on Macintoshes. This is the fourth edition of a book which first appeared in 1994, and the S environment has grown rapidly since. This book concentrates on using the current systems to do statistics; there is a companion volume  which discusses programming in the S language in much greater depth. Some of the more specialized functionality of the S environment is covered in on-line complements, additional sections and chapters which are available on the World Wide Web. The datasets and S functions that we use are supplied with most S environments and are also available on-line. This is not a text in statistical theory, but does cover modern statistical methodology.
--- papersmith/editor/grammar/grammar.py
+++ papersmith/editor/grammar/grammar.py
@@ -8,7 +8,15 @@ def check(content):
 	'检查内容中的语法错误'
 
 	# do something with the content, which is a string
-	issues = coref(content)
+	print("Loading spacy model")
+	try:
+		spacy.info('en_core_web_sm')
+		model = 'en_core_web_sm'
+	except IOError:
+		print("No spacy 2 model detected, using spacy1 'en' model")
+		model = 'en'
+	nlp = spacy.load(model)
+	issues = coref(content, nlp)
 	# Issue(category, itype, start(list), end(list), replacement, exp_id), 参见 ../issue.py
 
 	return issues  # List of issues
